f8e40a03fcd7be75e2c4f6bf33961a358f007642
jjones254 <justinjones254@gmail.com>
2021-11-03 09:09:29 -0500

    Update script.js
    hide other job role input field unless "Other" is selected

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // focus on name input field
 const nameInput = document.getElementById('name');
 nameInput.focus();
+
+// hides other job role input field unless "Other" is selected 
+const otherRoleInput = document.getElementById('other-job-role');
+const roleSelect = document.getElementById('title');
+otherRoleInput.hidden = true;
+roleSelect.addEventListener('change', () => {
+    if (roleSelect.value === 'other') {
+        otherRoleInput.hidden = false;
+    } else {
+        otherRoleInput.hidden = true;
+    };
+});
+
+
