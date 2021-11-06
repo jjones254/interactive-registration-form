ce242f9f42374bbb641a13f00867d8d969c41e60
jjones254 <justinjones254@gmail.com>
2021-11-05 22:15:58 -0500

    Update script.js
    Update the T-shirt color options based on the theme selected.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const nameInput = document.getElementById('name');
 nameInput.focus();
 
 // hides other job role input field unless "Other" is selected 
-const otherRoleInput = document.getElementById('other-job-role');
 const roleSelect = document.getElementById('title');
+const otherRoleInput = document.getElementById('other-job-role');
 otherRoleInput.hidden = true;
 roleSelect.addEventListener('change', () => {
     if (roleSelect.value === 'other') {
@@ -13,5 +13,26 @@ roleSelect.addEventListener('change', () => {
         otherRoleInput.hidden = true;
     };
 });
+
+// Updates T-shirt color options based on the theme selected
+const designSelect = document.getElementById('design');
+const colorSelect = document.getElementById('color');
+const colorOptions = colorSelect.children;
+colorSelect.disabled = true;
+designSelect.addEventListener('change', (e) => {
+    colorSelect.disabled = false;
+    for (const options of colorOptions) {
+        const theme = e.target.value;
+        const option = options.getAttribute('data-theme');
+        if (theme === option) {
+            options.hidden = false;
+            options.selected = true;
+        } else {
+            options.hidden = true;
+            options.selected = false;
+        };        
+    };
+});
+
 
 
